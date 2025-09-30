47dd11f6aa2ec8648ad71cd0651c4599914266a6
Gabriel-J-code <67019722+Gabriel-J-code@users.noreply.github.com>
2025-09-29 21:00:39 -0300

    feat(Fase): layout, pontos e colocação dos jogadores

---
+++ Elementos/Cenario/cenario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="cenario" tilewidth="64" tileheight="64" tilecount="4" columns="1">
+ <image source="cenario.png" width="90" height="300"/>
+</tileset>
